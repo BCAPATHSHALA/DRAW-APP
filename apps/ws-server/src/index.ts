5ef211632958195dbce8c190a904e2994e91877c
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2025-01-14 14:08:25 +0530

    complete steps 12

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,11 +1,26 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
 
-const ws = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8080 });
 
-ws.on("connection", function connection(ws) {
-//   console.log("Client connected");
+wss.on("connection", function connection(ws, req) {
+  const url = req.url; // ws://localhost:8080?token=123123
+
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]); // [" ws://localhost:8080", "token=123123"]
+  const token = queryParams.get("token") || ""; // "123123"
+  const decode = jwt.verify(token, JWT_SECRET); // {userId: 1}
+
+  if (!decode || !(decode as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
   ws.on("message", function incoming(message) {
-    // console.log("Received Message => ", message);
     ws.send("PONG MESSAGE: " + message);
   });
 });
