c93a37b0ec3be016846e774a17cdc8f8178f7de2
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2025-01-14 15:39:57 +0530

    complete step 15 for common package

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,12 +2,20 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware.js";
+import { CreateUserSchema } from "@repo/common/types";
 
 const app = express();
 
 // Signup Route
+
+//@ts-ignore Todo: Fix this
 app.post("/signup", async (req, res) => {
   // signup logic here
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.status(400).json({ message: "Invalid inputs" });
+  }
+  res.json({ message: "User created successfully" });
 });
 
 // Signin Route
